52aba2dae8c8000f686e8b39f8064eb1c58ab43b
keffri <keffri.w.neal@gmail.com>
2022-03-06 15:27:26 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import General from "./components/General";
+import General from "./components/General/General";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="app">
+        <General />
+      </div>
+    );
   }
 }
 
