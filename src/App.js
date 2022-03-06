cf39077cc6524aceacbab8b2809badfe1a033484
keffri <keffri.w.neal@gmail.com>
2022-03-06 14:04:58 -0800

    component setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div></div>;
+import General from "./components/General";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
 }
 
 export default App;
