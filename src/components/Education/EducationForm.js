195ddb582d34ddc6e82afc049d1b400408c856a6
keffri <keffri.w.neal@gmail.com>
2022-03-11 16:38:18 -0800

    education form

--- src/components/Education/EducationForm.js
+++ src/components/Education/EducationForm.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class EducationForm extends Component {
   render() {
     return (
-      <div>
+      <div className="education__form">
         <label>
           School:
           <input
