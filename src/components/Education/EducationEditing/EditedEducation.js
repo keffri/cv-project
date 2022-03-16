9effbf43c9acae8fa6e893485afc900ea66d6aa8
keffri <keffri.w.neal@gmail.com>
2022-03-15 19:01:54 -0700

    education rewrite functional component

--- src/components/Education/EducationEditing/EditedEducation.js
+++ src/components/Education/EducationEditing/EditedEducation.js
@@ -1,39 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 
-class EditedEducation extends Component {
-  render() {
-    return (
-      <div className="educationEdited">
-        <p className="educationEdited__p">
-          <span className="education--bold">School:</span>{" "}
-          {this.props.data.school}
-        </p>
-        <p className="educationEdited__p">
-          <span className="education--bold">Major:</span>{" "}
-          {this.props.data.major}
-        </p>
-        <p className="educationEdited__p">
-          <span className="education--bold">Start:</span>{" "}
-          {this.props.data.start}
-        </p>
-        <p className="educationEdited__p">
-          <span className="education--bold">End:</span> {this.props.data.end}
-        </p>
-        <button
-          className="educationEdited__button"
-          onClick={() => this.props.edit(this.props.id)}
-        >
-          Edit
-        </button>
-        <button
-          className="educationEdited__button"
-          onClick={() => this.props.delete(this.props.id)}
-        >
-          Delete
-        </button>
-      </div>
-    );
-  }
-}
+const EditedEducation = (props) => {
+  return (
+    <div className="educationEdited">
+      <p className="educationEdited__p">
+        <span className="education--bold">School:</span> {props.data.school}
+      </p>
+      <p className="educationEdited__p">
+        <span className="education--bold">Major:</span> {props.data.major}
+      </p>
+      <p className="educationEdited__p">
+        <span className="education--bold">Start:</span> {props.data.start}
+      </p>
+      <p className="educationEdited__p">
+        <span className="education--bold">End:</span> {props.data.end}
+      </p>
+      <button
+        className="educationEdited__button"
+        onClick={() => props.edit(props.id)}
+      >
+        Edit
+      </button>
+      <button
+        className="educationEdited__button"
+        onClick={() => props.delete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default EditedEducation;
